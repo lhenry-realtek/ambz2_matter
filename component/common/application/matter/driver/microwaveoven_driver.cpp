ae23ebbb73980cd0d5cd1401cb60c45c9e5663e2
xshuqun <xushuqun175@gmail.com>
2024-03-01 16:44:48 +0800

    [portinglayer] Add Microwave Oven Sample App
    * Add Microwave Oven Sample App

---
+++ component/common/application/matter/driver/microwaveoven_driver.cpp
@@ -0,0 +1,21 @@
+#include <microwaveoven_driver.h>
+#include <support/logging/CHIPLogging.h>
+
+void MatterMicrowaveOven::Init(PinName pin)
+{
+    mPwm_obj                        = (pwmout_t*) pvPortMalloc(sizeof(pwmout_t));
+    pwmout_init(mPwm_obj, pin);
+}
+
+void MatterMicrowaveOven::deInit(void)
+{
+    vPortFree(mPwm_obj);
+}
+
+void MatterMicrowaveOven::setOpState(uint8_t state)
+{
+    if (mOpState == state)
+        return;
+
+    mOpState = state;
+}
